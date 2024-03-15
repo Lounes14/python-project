c32c25d2ab4cafa474d3276962e51c33676760d0
Lounes14 <aklilounes2002@gmail.com>
2024-03-15 15:06:34 +0100

    FEAT(Development of different templates and updating of database)

--- schema.sql
+++ schema.sql
@@ -1,5 +1,17 @@
 CREATE TABLE users (
-                              id INT AUTO_INCREMENT PRIMARY KEY,
+                              id INTEGER PRIMARY KEY AUTOINCREMENT,
                               username VARCHAR(50) NOT NULL UNIQUE,
                               password VARCHAR(100) NOT NULL
+);
+CREATE TABLE questions (
+                           id INTEGER PRIMARY KEY AUTOINCREMENT,
+                           question_text VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE answers (
+                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                         question_id INT,
+                         answer_text VARCHAR(255) NOT NULL,
+                         is_correct BOOLEAN,
+                         FOREIGN KEY (question_id) REFERENCES questions(id)
 );
