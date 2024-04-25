b9ba74fcdc9750df95054154b983acc033b7bdae
Lounes14 <aklilounes2002@gmail.com>
2024-04-25 12:29:38 +0200

    FEAT(inserting data into sql)

--- schema.sql
+++ schema.sql
@@ -15,3 +15,25 @@ CREATE TABLE answers (
                          is_correct BOOLEAN,
                          FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (1, 'Paris', TRUE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (1, 'Rome', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (1, 'Tokyo', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (2, 'Alger', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (2, 'Tokyo', TRUE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (2, 'Chicago', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (3, 'Elon Musk', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (3, 'Bill Gates', TRUE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (3, 'Larry Page', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (4, 'Tik Tok', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (4, 'Facebook', TRUE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (4, 'X', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (5, 'Anglais', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (5, 'Espagnol', TRUE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (5, 'Allemand', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (6, 'RAM', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (6, 'CPU', TRUE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (6, 'ROM', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (7, 'PHP', FALSE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (7, 'JavaScripts', TRUE);
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES (7, 'Python', FALSE
